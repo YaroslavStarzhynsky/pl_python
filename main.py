430dc8b7b0aaebe55215b2ea78df3039e7432a85
Yaroslav <93600283+YaroslavStarzhynsky@users.noreply.github.com>
2022-10-13 10:28:03 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
-import wikipediaapi
-wiki_wiki = wikipediaapi.Wikipedia('pl')
-page_py = wiki_wiki.page('Kapibara_wielka')
-print(page_py.links)
-for key in page_py.links:
-    print(key)
-    next_page = wiki_wiki.page(key)
-    print(next_page.links)
+plik = open("przyklad.txt",'r')
+dane = plik.read().split("\n\n")
+dzialki = []
+for line in dane:
+    lineso = line.split("\n")
+    dzialki.append(lineso)
+print(dzialki)
+counter = 0
+countertraw = 0
+for i in range(len(dane)):
+    for j in range(0, len(dane[i])):
+        if(dane[i][j] == '*'):
+            counter+=1
+    if(counter/900>=0.7):
+        countertraw+=1
+        counter = 0
+
+print(countertraw)
+
+
+# 4.2
+
+square = [0,0]
+for i in range(len(dzialki)):
+    temp = dzialki[i]
+    rotate = ""
+    for i in range()
