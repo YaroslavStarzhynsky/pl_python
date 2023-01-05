eb8f4aae4e723acfa333459b48e912f908099545
Yaroslav <93600283+YaroslavStarzhynsky@users.noreply.github.com>
2023-01-05 10:40:17 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,42 +1,32 @@
-def poprawna(pesel):
-    suma = 0
-    suma = (int(pesel[0])*1 + int(pesel[1])*3 + int(pesel[2])*7 + int(pesel[3])*9 + int(pesel[4])*1 + int(pesel[5])*3 + int(pesel[6])*7+int(pesel[7])*9+int(pesel[8])*1+ int(pesel[9])*3+int(pesel[10]))
-    return suma
+def dilniki(a):
+    resulti = 0
+    k = 3
+    if a % 2==0:
+        return False
+    while(a > 1):
+        if a % k == 0:
+            resulti +=1
+        while(a % k==0):
+            a = a//k
+        k = k+2
+    if resulti > 3 or resulti<3:
+        return False
+    if resulti == 3:
+        return True
 
-file = open("przyklad.txt", "r")
-pesel = []
-with open("dane.txt", "r") as f:
-    for line in f.readlines():
-        pesel.append(line)
-
-#zadanie 6_1
-kobiet = 0
-mezczyzn = 0
-with open("dane.txt", "r") as f:
-    for line in f.readlines():
-        if(int(line[9])%2==0):
-            kobiet+=1
-        else:
-            mezczyzn+=1
-print(kobiet, mezczyzn)
-# 442 558
-
-#zadanie 6_2
+plik = "liczby.txt"
 counter = 0
-for i in pesel:
-    m = int(i[2:4])
-    if m > 12:
-        m=m-20
-    if(m==11):
+a = []
+with open(plik) as file:
+    for line in file:
+        a.append(int(line[:-1]))
+print(a)
+for i in range(len(a)):
+    if (dilniki(a[i]) == True):
         counter+=1
-
 print(counter)
-#90
 
-#zadanie 6_3
-blend = []
-with open("dane.txt", "r") as f:
-    for line in f.readlines():
-        if(poprawna(line)%10!=0):
-            blend.append(line)
-print(blend)
+#wynik 59.1 - 113
+#zadanie 59.2
+
+for i in range(len(a))
